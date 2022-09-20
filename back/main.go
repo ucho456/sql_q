20cdec8c262b5fe8e065f3590c5d136062722cb3
ucho456 <watapqxyz@gmail.com>
2022-09-20 16:14:08 +0900

    テストの為に、run関数へ処理を分離する

--- back/main.go
+++ back/main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":8080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, world!")
-		}),
-	)
-	if err != nil {
+	if err := run(context.Background()); err != nil {
 		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
 	}
+}
+
+func run(ctx context.Context) error {
+	srv := &http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "Hello, world!")
+		}),
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	// 別ゴルーチンでhttpサーバーを起動する
+	eg.Go(func() error {
+		if err := srv.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// チャネルからの通知（終了通知）を待機する
+	<-ctx.Done()
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// Goメソッドで起動した別ゴルーチンの終了を待つ
+	return eg.Wait()
 }
